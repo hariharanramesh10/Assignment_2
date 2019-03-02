7ce5333a219d5e5a146a1c0cff769fbb2e3ac3b8
smnsingh444 <46601093+smnsingh444@users.noreply.github.com>
2019-03-01 21:15:40 -0400

    Add files via upload

--- Assignment_2/EmployerPortalApplication/src/main/java/com/macs/groupone/employerportalapplication/service/EmployeeService.java
+++ Assignment_2/EmployerPortalApplication/src/main/java/com/macs/groupone/employerportalapplication/service/EmployeeService.java
@@ -3,13 +3,17 @@ package com.macs.groupone.employerportalapplication.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.macs.groupone.employerportalapplication.domain.Employee;
+import com.macs.groupone.employerportalapplication.domain.MortgageApplicant;
 import com.macs.groupone.employerportalapplication.repository.EmployeeRepository;
 
 // employee service class
 @Service
 public class EmployeeService {
+	
+	static final String URL_EMPLOYEE = "http://localhost:8086/morgageaaplicant/";
 	
 	EmployeeService()
 	{
@@ -24,6 +28,18 @@ public class EmployeeService {
 		Employee employee=employeeRepository.getEmployeeByEmailAndPassword(email, password);
 		return employee;
 	}
+	
+	// get employee by email id and password
+		public Boolean getEmployeeByApplicationNumber(String applicationNumber,String link) {
+			RestTemplate restTemplate = new RestTemplate();
+	        // Send request with GET method and default Headers.
+	        MortgageApplicant applicant = restTemplate.getForObject(URL_EMPLOYEE+applicationNumber, MortgageApplicant.class);
+	        if (applicant != null) {
+	                System.out.println("Employee: " + applicant.getFirstName() + " - " + applicant.getLastName()); 
+	                return true;
+	        }
+			return false;
+		}
 
 	
 }
